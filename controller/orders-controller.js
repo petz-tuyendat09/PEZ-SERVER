4885ac35c19effa567cafe6a2a69904110c3d447
thunguyet09 <thnguyet03@gmail.com>
2024-10-27 12:19:03 +0700

    27/10

--- controller/orders-controller.js
+++ controller/orders-controller.js
@@ -1,3 +1,4 @@
+const Order = require("../models/Order");
 const orderServices = require("../services/orderServices");
 
 exports.getOrderByUserId = async (req, res) => {
@@ -9,3 +10,40 @@ exports.getOrderByUserId = async (req, res) => {
     console.log(error);
   }
 };
+
+exports.insertOrders = async (req, res) => {
+  try {
+    const { 
+      customerName, 
+      customerPhone, 
+      customerEmail, 
+      customerAddress, 
+      productId,
+      orderTotal, 
+      voucherId,
+      orderDiscount, 
+      userId, 
+      totalAfterDiscount, 
+      paymentMethod, 
+      orderStatus
+    } = req.body
+    const OrderModel = new Order({
+      customerName, 
+      customerPhone, 
+      customerEmail, 
+      customerAddress, 
+      productId,
+      orderTotal, 
+      voucherId,
+      orderDiscount, 
+      userId, 
+      totalAfterDiscount, 
+      paymentMethod, 
+      orderStatus
+    })
+    const savedOrder = await OrderModel.save();
+    return res.status(200).json({ success: true, data: savedOrder })
+  } catch (error) {
+    console.log(error);
+  }
+}
--- routes/orders.routes.js
+++ routes/orders.routes.js
@@ -3,5 +3,5 @@ const router = express.Router();
 const orderController = require("../controller/orders-controller");
 
 router.get("/order-userId/", orderController.getOrderByUserId);
-
+router.post("/", orderController.insertOrders);
 module.exports = router;
